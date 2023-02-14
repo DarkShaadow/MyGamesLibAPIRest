a7a82042c4861422b153476575ffa53d08a77193
Antony Stark <darkshaadowdev@gmail.com>
2023-02-14 21:19:08 +0100

    Initialisation

---
+++ src/main/java/fr/ul/mygameslibapirest/MyGamesLibApiRestApplication.java
@@ -0,0 +1,13 @@
+package fr.ul.mygameslibapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGamesLibApiRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyGamesLibApiRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/ul/mygameslibapirest/MyGamesLibApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ul.mygameslibapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGamesLibApiRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
