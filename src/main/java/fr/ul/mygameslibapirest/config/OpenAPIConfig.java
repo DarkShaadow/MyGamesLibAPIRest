c33d674e66d4158d3fb279bd27f9588d25e16638
DarkShaadow <darkshaadowdev@gmail.com>
2023-02-21 13:33:24 +0100

    Ajout du swagger

--- src/main/java/fr/ul/mygameslibapirest/config/OpenAPIConfig.java
+++ src/main/java/fr/ul/mygameslibapirest/config/OpenAPIConfig.java
@@ -3,7 +3,7 @@ package fr.ul.mygameslibapirest.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import org.springdoc.core.SpringDocUtils;
+import org.springdoc.core.utils.SpringDocUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
